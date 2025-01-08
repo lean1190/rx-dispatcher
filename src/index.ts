7a1cfb5ba5be0d9e9662c5cf9a502559b32fb92a
Lean <leian1306@gmail.com>
2025-01-08 17:55:08 -0300

    Add export

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { createActionDispatcher } from './dispatcher/dispatcher';
+export * from './types/types';
+export { ofType } from './utils/ofType';
+export { takeValues } from './utils/takeValues';
